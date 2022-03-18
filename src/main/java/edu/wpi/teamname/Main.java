bdf50974be6b0599b2d2e4a87d31cdf0e7995f4a
jjbornstein <jjbornstein@wpi.edu>
2022-03-18 12:39:44 -0400

    initial commit

---
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -0,0 +1,10 @@
+package edu.wpi.teamname;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Before Launch");
+    App.launch(App.class, args);
+    System.out.println("After Launch");
+  }
+}
