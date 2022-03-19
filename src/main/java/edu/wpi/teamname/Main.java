795dbe4556f4d500e53875a0c4383a9ee7c0a1d9
ngrozdani <64702995+ngrozdani@users.noreply.github.com>
2022-03-18 23:20:06 -0400

    added my name Nikola

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,6 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("Nikola");
     System.out.println("Jeremy");
     System.out.println("Before Launch");
     App.launch(App.class, args);
